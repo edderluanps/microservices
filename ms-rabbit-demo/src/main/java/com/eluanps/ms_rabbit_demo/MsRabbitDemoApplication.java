31641dc6e7cc496a8508b75a910ad18eff689290
edderluanps <edder.pessoa@gaiacode.com.br>
2024-12-23 23:40:30 -0300

    MS API/RabbitMQ Backend

---
+++ ms-rabbit-demo/src/main/java/com/eluanps/ms_rabbit_demo/MsRabbitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eluanps.ms_rabbit_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRabbitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRabbitDemoApplication.class, args);
+	}
+
+}
---
+++ ms-rabbit-demo/src/test/java/com/eluanps/ms_rabbit_demo/MsRabbitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluanps.ms_rabbit_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRabbitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
