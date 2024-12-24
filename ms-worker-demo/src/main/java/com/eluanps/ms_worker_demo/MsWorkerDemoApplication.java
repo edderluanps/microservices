86eeb876f8fa7e34b4d531d40c29ff52eaff252c
edderluanps <edder.pessoa@gaiacode.com.br>
2024-12-23 23:42:12 -0300

    MS API/Worker Backend

---
+++ ms-worker-demo/src/main/java/com/eluanps/ms_worker_demo/MsWorkerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eluanps.ms_worker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWorkerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWorkerDemoApplication.class, args);
+	}
+
+}
---
+++ ms-worker-demo/src/test/java/com/eluanps/ms_worker_demo/MsWorkerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluanps.ms_worker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWorkerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
